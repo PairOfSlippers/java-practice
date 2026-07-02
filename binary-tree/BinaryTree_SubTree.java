9bcf38373d0cecbc2798904641225eff0e38872d
PairOfSlippers <llamafluffy5@gmail.com>
2026-07-02 16:48:30 +0530

    Fixed initial condition

--- binary-tree/BinaryTree_SubTree.java
+++ binary-tree/BinaryTree_SubTree.java
@@ -1,3 +1,5 @@
+// LeetCode #572 - SubTree of Another Tree.
+// Time : O(n^2) || Space : O(h)
 public class BinaryTree_SubTree {
     static class Node {
         int data;
@@ -42,7 +44,8 @@ public class BinaryTree_SubTree {
                 return false;
             }
             if (root.data == subroot.data) {
-                return isIdentical(root, subroot);
+                if(isIdentical(root, subroot))
+                    return true;
             }
             return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
         }
